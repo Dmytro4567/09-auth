ac3ae6cf1e85e64b203eb156cbf11b1123bfa521
Dmytro <dmitry.dvernikov@gmail.com>
2025-07-19 15:37:53 +0300

    fix lib/store/authStore.ts

--- lib/store/authStore.ts
+++ lib/store/authStore.ts
@@ -1,6 +1,6 @@
-import {User} from "@/types/user";
-import {create} from "zustand";
-import {getMe} from "@/lib/api/clientApi";
+import { User } from "@/types/user";
+import { create } from "zustand";
+import { getMe } from "@/lib/api/clientApi";
 
 type AuthStore = {
     isAuthenticated: boolean;
@@ -10,21 +10,27 @@ type AuthStore = {
     checkAuth: () => Promise<void>;
 };
 
-export const useAuthStore = create<AuthStore>((set) => ({
-    isAuthenticated: false,
-    user: null,
-    setUser: (user: User) => {
-        set(() => ({user, isAuthenticated: true}));
-    },
-    clearIsAuthenticated: () => {
-        set(() => ({user: null, isAuthenticated: false}));
-    },
-    checkAuth: async () => {
-        try {
-            const user = await getMe();
-            set({user, isAuthenticated: true});
-        } catch {
-            set({user: null, isAuthenticated: false});
-        }
-    },
-}));
+export const useAuthStore = create<AuthStore>()(
+    (set) => ({
+        isAuthenticated: false,
+        user: null,
+
+        setUser: (user: User) => {
+            set(() => ({ user, isAuthenticated: true }));
+        },
+
+        clearIsAuthenticated: () => {
+            set(() => ({ user: null, isAuthenticated: false }));
+        },
+
+        checkAuth: async () => {
+            try {
+                const user = await getMe();
+                set({ user, isAuthenticated: true });
+            } catch {
+                set({ user: null, isAuthenticated: false });
+            }
+        },
+    })
+);
+
